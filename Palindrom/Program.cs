67b049f053271452e77510a0e95c66c530288526
sametkapar <130076921+sametkapar@users.noreply.github.com>
2024-07-08 16:23:58 +0300

    Palindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ada, mum, kabak
+            Console.WriteLine("Bir kelime giriniz");
+            
+            string metin = Console.ReadLine();
+            metin = metin.ToLower();
+            string ters = "";
+            for (int i = (metin.Length - 1); i >= 0; i--)
+            {
+                ters = ters + metin[i];
+
+            }
+            Console.WriteLine(metin + " kelimesininin tersten yazılışı: " + ters );
+
+            if (metin == ters)
+            {
+                Console.WriteLine(metin + " kelimesi bir palindrom kelimedir.");
+            }
+            else
+            {
+                Console.WriteLine(metin+" kelimesi palindrom kelime değildir.");
+            }
+
+        }
+    }
+}
